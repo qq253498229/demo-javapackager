03422a1e80eb3ef876d653e269220f5d5f268a13
大胃王 <codeforfun@foxmail.com>
2022-08-01 09:49:21 +0800

    init

---
+++ src/test/java/com/example/DemoJavapackagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJavapackagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
